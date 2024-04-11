b0b10691f60f7484bdbdc65540a9124fb963f260
Bal Gopal Lal Shrestha <03bgls25@gmail.com>
2024-04-11 15:10:52 +0545

    prisma issues hit and try

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ type Book = {
 }
 
 const getAllBooks = async () => {
-  const res = await fetch(`${ process.env.NEXT_PUBLIC_API }/api/books`)
+  const res = await fetch(`${ process.env.NEXT_PUBLIC_API }/books`)
   const data = await res.json();
   return data.books;
 }
